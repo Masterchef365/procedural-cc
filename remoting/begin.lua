523d0a8cbd1483a6f4914b495feef0af6c9f7283
Masterchef365 <duncan.freeman1@gmail.com>
2024-07-29 21:47:53 -0700

    Xor stuff

--- remoting/begin.lua
+++ remoting/begin.lua
@@ -1,5 +1,7 @@
 shell.run("remoterun.lua", "remoting/identifier.lua")
 sleep(1) -- Required to let the workers start
 shell.run("identify_workers.lua")
-shell.run("remoterun.lua", "remoting/voronoi.lua")
+
+--shell.run("remoterun.lua", "remoting/voronoi.lua")
 --shell.run("remoterun.lua", "remoting/showpos.lua")
+shell.run("remoterun.lua", "remoting/xor_pattern.lua")
